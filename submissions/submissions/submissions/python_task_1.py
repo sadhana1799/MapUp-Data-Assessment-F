d198f7ec7b5455a0dd96a94bc4866b7f04967a10
Sadhana Suman <140735185+sadhana1799@users.noreply.github.com>
2023-12-09 21:09:08 +0530

    Update python_task_1.py

--- submissions/submissions/submissions/python_task_1.py
+++ submissions/submissions/submissions/python_task_1.py
@@ -1,1 +1,16 @@
+import pandas as pd
+
+
+def generate_car_matrix(df)->pd.DataFrame:
+    car_matrix = df.pivot(index='id_1', columns='id_2', values='car')
+    car_matrix = car_matrix.fillna(0)
+    for i in range(min(car_matrix.shape)):
+        car_matrix.iloc[i, i] = 0
+
+    return car_matrix
+
+df = pd.read_csv("dataset-1 assessment.csv")
+generate_car_matrix(df)
+
+
 
